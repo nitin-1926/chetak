a362f4cfab5e4acb5a255552ca45ec41bbe3d870
Nitin Gupta <nitin@rocketium.com>
2025-04-18 20:54:23 +0530

    chore: removed old files

--- src/reducer.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { userReducer } from './modules/Profile/reducers';
-
-// Combine reducers
-const rootReducer = combineReducers({
-	user: userReducer,
-});
-
-export default rootReducer;
